2331bb9e7853884b161481e8466e518e49e49439
BAO TRANG <baogtrang@gmail.com>
2024-12-14 16:47:01 -0600

    bootstrap

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,58 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./DisplayInfo.scss";
 import logo from "./../logo.svg";
-
-//stateless vs stateful
-// class DisplayInfo extends React.Component {
-//     // state = {
-//     //     isShowUserList: true
-//     // }
-
-//     render() {
-//         console.log("Call me render")
-//         const { userList } = this.props;
-//         return (
-//             <div className="display-info-container">
-
-//                 {true &&
-//                     <>
-//                         {userList.map((user, index) => {
-//                             console.log('userList:', userList)
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                     <div>
-//                                         <div >My name is {user.name}</div>
-//                                         <div>I am {user.age} years old</div>
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => {
-//                                             this.props.handleDeleteUser(user.id)
-//                                         }}>
-//                                             Delete
-//                                         </button>
-//                                     </div>
-//                                     <hr />
-//                                 </div>
-//                             )
-//                         })
-//                         }
-//                     </>
-//                 }
-//             </div>
-
-//         )
-//     }
-// }
 
 const DisplayInfo = (props) => {
     const { userList } = props;
     const [isShowHideUserList, setShowHideUserList] = useState(true);
-    // this.state = {
-    //     isShowHideUserList: true
-    // }
     const handleShowHideList = () => {
         setShowHideUserList(!isShowHideUserList);
     }
+    console.log(">>> call Render");
+
+    useEffect(
+        () => {
+            if (userList.length === 0) {
+                alert("You deleted all the users")
+            }
+            console.log(">>> call useEffect");
+        }, [userList]
+    );
     return (
         <div className="display-info-container">
             <div>
